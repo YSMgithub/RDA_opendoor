b3c076ec5cf339d4b929b5e05fd4e57d381bef61
Сергей Юрченко <s.yurchenko@sibset-office.211.ru>
2023-04-28 18:05:22 +0700

    add init struct

--- src/dooropener.c
+++ src/dooropener.c
@@ -2,4 +2,25 @@
 #include <stdlib.h>
 #include <curl/curl.h>
 #include <string.h>
+#include "dooropener.h"
 
+void init_struct(data_t *data) {
+  data->rdaID = malloc(MAX_NAME_LENGTHS);
+  data->user = malloc(MAX_NAME_LENGTHS);
+  data->password = malloc(MAX_NAME_LENGTHS);
+  data->token = malloc(MAX_TOKEN_LENGTHS);
+  if (data->rdaID == NULL || data->rdaID == NULL || data->rdaID == NULL || data->rdaID == NULL) {
+    fprintf(stderr, "malloc() failed\n");
+    exit(EXIT_FAILURE);
+  }
+  data->rdaID[0] = '\0';
+  data->user[0] = '\0';
+  data->password[0] = '\0';
+  data->token[0] = '\0';
+
+}
+
+int main() {
+    data_t data;
+    return 0;
+}
--- src/dooropener.h
+++ src/dooropener.h
@@ -1,7 +1,8 @@
 #ifndef SRC_DOOROPENER_H_
 #define SRC_DOOROPENER_H_
 
-#define WIDTH 160
+#define MAX_NAME_LENGTHS 160
+#define MAX_TOKEN_LENGTHS 512
 
 typedef struct data_s {
     char * rdaID;
