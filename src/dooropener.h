15546caee5bd4d9002513540ad5e93f3b53b3e12
Сергей Юрченко <s.yurchenko@sibset-office.211.ru>
2023-04-28 17:48:29 +0700

    start

---
+++ src/dooropener.h
@@ -0,0 +1,13 @@
+#ifndef SRC_DOOROPENER_H_
+#define SRC_DOOROPENER_H_
+
+#define WIDTH 160
+
+typedef struct data_s {
+    char * rdaID;
+    char * user;
+    char * password;
+    int * token;
+} data_t;
+
+#endif  // SRC_DOOROPENER_H_"
