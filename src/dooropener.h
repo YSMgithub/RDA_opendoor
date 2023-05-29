e91a71d3819c02077254a1ad4cb3422c7548180d
Сергей Юрченко <s.yurchenko@sibset-office.211.ru>
2023-05-29 18:18:00 +0700

    style edit

--- src/dooropener.h
+++ src/dooropener.h
@@ -4,41 +4,39 @@
 #define MAX_NAME_LENGTHS 160
 #define MAX_TOKEN_LENGTHS 800
 
-typedef struct flags_s {
+typedef struct flags_s
+{
     int dataChgFlg;
     int doorOpen;
     int getOrPost;
 } flags_t;
 
-typedef struct data_s {
-    char * rdaID;
-    char * user;
-    char * password;
-    char * token;
-    char * chunk;
+typedef struct data_s
+{
+    char *rdaID;
+    char *user;
+    char *password;
+    char *token;
+    char *chunk;
     flags_t flags;
 } data_t;
 
+void initStruct(data_t *data_s);
 
+void dataReader(data_t *data_s);
 
-
-
-void initStruct(data_t * data_s);
-
-void dataReader(data_t * data_s);
-
-int gettokenfunc(data_t * data_s);
+int gettokenfunc(data_t *data_s);
 
 size_t writefunc(void *ptr, size_t size, size_t nmemb, data_t *data_s);
 
-int sendPostFunc (data_t *data_s);
+int sendPostFunc(data_t *data_s);
 
-int writeToFile (data_t *data_s);
+int writeToFile(data_t *data_s);
 
-void readFromFile (data_t *data_s);
+void readFromFile(data_t *data_s);
 
-int getArgs (int argc, char *argv[], data_t *data_s);
+int getArgs(int argc, char *argv[], data_t *data_s);
 
-void memFree (data_t *data_s);
+void memFree(data_t *data_s);
 
-#endif  // SRC_DOOROPENER_H_"
+#endif // SRC_DOOROPENER_H_"
